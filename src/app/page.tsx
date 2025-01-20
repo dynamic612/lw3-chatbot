476f15c3c9344c4b3f102a637313529bed1f0c0f
Blockchainer <dynamic612strong@outlook.com>
2025-01-20 06:52:18 +0200

    fixing Emter to Enter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,7 +37,7 @@ export default function Home() {
   };
 
   const Submit = (event: React.KeyboardEvent<HTMLTextAreaElement>) => {
-    if (event.key === "Emter") {
+    if (event.key === "Enter") {
       event?.preventDefault();
       callGetResponse();
     }
